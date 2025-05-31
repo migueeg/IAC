0c36f6c3ca070df012db5cbed99b52ff92fed910
FranciscoAroni <faronim1@upao.edu.pe>
2025-05-30 23:48:36 -0500

    feat: agrega Lambda register_event y DynamoDB con Terraform

--- iac/iam-lambda.tf
+++ iac/iam-lambda.tf
@@ -41,3 +41,9 @@ resource "aws_iam_role_policy_attachment" "lambda_logs_attach" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+# Permitir que la Lambda acceda a DynamoDB
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
