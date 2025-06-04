fbf4b61e1087e889b65abd295c5e2ca96cfa28de
Miguel Angel Escobar Gomez <maeg2468@gmail.com>
2025-06-03 21:55:56 -0500

    implementando kinesis para recibir logs

--- iac/iam-lambda.tf
+++ iac/iam-lambda.tf
@@ -72,3 +72,9 @@ resource "aws_iam_role_policy_attachment" "lambda_ses_access" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
 }
+
+# Permitir que la Lambda acceda a Kinesis
+resource "aws_iam_role_policy_attachment" "lambda_kinesis_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
+} 
