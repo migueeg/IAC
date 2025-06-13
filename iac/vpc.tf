2a3e271a307ce58e28a0257211abf42d75d0a43c
FranciscoAroni <faronim1@upao.edu.pe>
2025-06-13 17:26:41 -0500

    Fixed VPC subnet public IP assignment

--- iac/vpc.tf
+++ iac/vpc.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a"
-  map_public_ip_on_launch = true  # Auto-assign public IP
+  map_public_ip_on_launch = False  # Auto-assign public IP
 
   tags = {
     Name = "public-subnet-a"
@@ -34,7 +34,7 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-2b"
-  map_public_ip_on_launch = true  # Auto-assign public IP
+  map_public_ip_on_launch = False  # Auto-assign public IP
 
   tags = {
     Name = "public-subnet-b"
