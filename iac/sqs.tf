6560dbaafc87c054520a73b13c62126b368d8519
FranciscoAroni <faronim1@upao.edu.pe>
2025-06-13 19:48:24 -0500

    Enable encryption for all SQS queues to comply with AWS security policies

--- iac/sqs.tf
+++ iac/sqs.tf
@@ -1,11 +1,21 @@
 resource "aws_sqs_queue" "event_queue" {
   name                      = "event-queue"
   visibility_timeout_seconds = 30
+  
+  # Habilitar el cifrado utilizando la clave predeterminada de SQS
+  kms_master_key_id = "alias/aws/sqs"
 }
+
 resource "aws_sqs_queue" "lambda_dlq" {
   name = "lambda-loginUser-dlq"
+  
+  # Habilitar el cifrado utilizando la clave predeterminada de SQS
+  kms_master_key_id = "alias/aws/sqs"
 }
 
 resource "aws_sqs_queue" "lambda_dlq_register_event" {
   name = "lambda-dlq-register-event"
+  
+  # Habilitar el cifrado utilizando la clave predeterminada de SQS
+  kms_master_key_id = "alias/aws/sqs"
 }
