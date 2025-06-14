15a95e276fc87e5221d3d97f57ef76431a642532
JesusLimay14 <jlimayc1@upao.edu.pe>
2025-06-14 11:14:16 -0500

    Solucionando Error 64

--- iac/kms.tf
+++ iac/kms.tf
@@ -1,8 +1,41 @@
+# Identidad de la cuenta actual para políticas KMS
+data "aws_caller_identity" "current" {}
+
 # Configuración de claves KMS para las variables de entorno de Lambda
 resource "aws_kms_key" "lambda_environment_kms" {
   description             = "KMS key for Lambda environment variables"
   enable_key_rotation     = true
   deletion_window_in_days = 10
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "key-default-1",
+    Statement = [
+      {
+        Sid    = "AllowRootAccount",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*",
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowLambdaUse",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.lambda_exec_role.arn
+        },
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 # Configuración de claves KMS para el frontend
