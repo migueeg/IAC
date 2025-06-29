7216214fa3ad66b6e6cb9d6d9420ab90a32a98f3
JesusLimay14 <jlimayc1@upao.edu.pe>
2025-06-29 18:19:55 -0500

    Solucionando error 43

--- iac/kinesis.tf
+++ iac/kinesis.tf
@@ -1,8 +1,12 @@
 resource "aws_kinesis_stream" "event_stream" {
-  name             = "event-stream"
-  shard_count      = 1
-  retention_period = 24
-  shard_level_metrics = ["IncomingBytes", "OutgoingBytes"]
+  name                 = "event-stream"
+  shard_count          = 1
+  retention_period     = 24
+  shard_level_metrics  = ["IncomingBytes", "OutgoingBytes"]
+
+  # Añadido para habilitar cifrado seguro en reposo
+  encryption_type = "KMS"
+  kms_key_id      = "alias/aws/kinesis"
 
   tags = {
     Environment = "dev"
