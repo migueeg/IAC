a25ce62957c96e9ccf95879edf500ff979c593f2
JesusLimay14 <jlimayc1@upao.edu.pe>
2025-06-29 20:15:21 -0500

    Solucionando error 157

--- iac/rds.tf
+++ iac/rds.tf
@@ -1,22 +1,25 @@
 # Instancia de PostgreSQL 
 resource "aws_db_instance" "postgres_db" {
   identifier           = "eventos-db"
-  engine              = "postgres"
-  engine_version      = "14"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 20
-  publicly_accessible    = true
+  engine               = "postgres"
+  engine_version       = "14"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  publicly_accessible  = true
 
-  db_name             = "eventosdb"
-  username            = var.db_username
-  password            = var.db_password
+  db_name              = "eventosdb"
+  username             = var.db_username
+  password             = var.db_password
+
+  # Habilitar Multi-AZ
+  multi_az             = true
   
   # Configuración de VPC
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   
   skip_final_snapshot = true
-  
+
   tags = {
     Environment = var.environment
   }
